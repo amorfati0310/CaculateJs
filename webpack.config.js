0513ec3b1b6943c4af7df3b3acc057086311b11d
chany <lyhy0310@gmail.com>
2019-03-18 00:49:13 +0900

    report: sass Issue webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,36 @@ const config = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-
+    rules: [
+        {
+            test: /\.js$/,
+            use: {
+                loader: "babel-loader",
+                options: { presets: ["es2015"] }
+            }
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
+                    loader: "css-loader" // translates CSS into CommonJS
+                },
+                {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }
+            ]
+        }
+    ],
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Project Demo',
             minify: {
-                collapseWhitespace: true
+                collapseWhitespace: true // 문서의 텍스트 노드에서 공백을 제거합니다.
             },
-            hash: true,
+            hash: true, //  CSS 혹은 JS 파일에 고유한 웹팩 컴파일 해시를 추가합니다
             template: './src/index.html'
         })
     ],
